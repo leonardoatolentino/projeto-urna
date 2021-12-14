ce0bf384bde53393adfbb2e9994cfaa36fddac79
leonardoatolentino <91916209+leonardoatolentino@users.noreply.github.com>
2021-12-14 11:09:33 -0300

    Add files via upload

---
+++ urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+      titulo: 'VEREADOR',
+      numeros: 5,
+      candidatos: [
+          {
+              numero: '38111',
+              nome: 'Papa-Léguas',
+              partido: 'Foguete',
+              fotos: [
+                  {url: '38111.jpg', legenda: 'Vereador'}
+              ]
+          },
+          {
+              numero: '77222',
+              nome: 'Pato Donald',
+              partido: 'Mal-humorado',
+              fotos: [
+                  {url: '77222.jpg',legenda: 'Vereador'}
+              ]
+          }
+      ]
+  },
+  {
+      titulo: 'PREFEITO',
+      numeros: 2,
+      candidatos: [
+          {
+              numero: '84',
+              nome: 'Pica-Pau',
+              partido: 'Bonzinhos',
+              vice: 'Leoncio',
+              fotos: [
+                  {url: '84.jpeg', legenda: 'Prefeito'},
+                  {url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+              ]
+          },
+          {
+              numero: '99',
+              nome: 'Zeca-Urubu',
+              partido: 'Malvado',
+              vice: 'ZuDick-Vigaristal',
+              fotos: [
+                  {url: '99.jpg', legenda: 'Prefeito'},
+                  {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+              ]
+          }
+      ]
+  }
+];
